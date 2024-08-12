8738158623d2d3b79598da5eaf9e695e673d277e
johnzhuang23 <johnzhuang.work@gmail.com>
2024-08-12 19:33:40 +1000

    make a commit manually

--- testscript.ps1
+++ testscript.ps1
@@ -1,3 +1,11 @@
+# PARAM(
+
+#     [Parameter(Mandatory = $true)]$var = ""
+
+# )
+
+# [string]$content = "test", $var -join "-"
+
 $content = Get-Date
 
 Set-Content -Path "content.md" -Value $content
